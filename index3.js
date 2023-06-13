48cc5ee11d797adef4194045a5b2e5b92b8a5ee8
TaniquaPilar <chattelboards@gmail.com>
2023-06-13 01:37:34 -0400

    Update June 13
    created navbar.js
    created navbar.css
    created question.js for accordion questions
    created how-it-works.html to give answers of the shopping experience
    added navbar and footer to index.html, mail.html
    added questions to index.html
    updated index3.css, index.js

--- index3.js
+++ index3.js
@@ -32,3 +32,66 @@ slides[slidePosition-1].style.display= "block";
 circles[slidePosition-1].className+= "enable";
 setTimeout(showSlides,3000); //change imaage every 3 seconds
 }
+
+
+
+//Start questions
+//using selectors inside the element
+//const questions = document.querySelectorAll(".question");
+
+//questions.forEach(function (question) {
+  //const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  //btn.addEventListener("click", function () {
+    // console.log(question);
+
+    //questions.forEach(function (item) {
+    //if (item !== question) {
+    //  item.classList.remove("show-text");
+    //}
+    //});
+
+    //question.classList.toggle("show-text");
+ //});
+//});
+
+// traversing the dom
+ //const btns = document.querySelectorAll(".question-btn");
+
+ //btns.forEach(function (btn) {
+    //btn.addEventListener("click", function (e) {
+    // const question = e.currentTarget.parentElement.parentElement;
+
+     //question.classList.toggle("show-text");
+   //});
+ //});
+
+
+ //var countDownDate = new Date("June 1, 2023 00:00:00").getTime();
+ //var x = setInterval(function(){
+  //var now = new Date().getTime();
+  //var distance = countDownDate - now;
+
+  //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //var hours= Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //var minutes= Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //var seconds= Math.floor((distance % (1000 * 60)) / (1000);
+
+ //document.getElementById("demo").innerHTML=
+ //days+ "d" + hours + "h" + minutes + "m" + seconds + "s";
+  //document.getElementById("days").innerHTML= days;
+  //document.getElementById("hours").innerHTML= hours;
+  //document.getElementById("minutes").innerHTML= minutes;
+  //document.getElementById("seconds").innerHTML= seconds;
+
+  //if(distance < 0){
+    //clearInterval(x);
+    //document.getElementById("demo").innerHTML= "00"
+    //document.getElementById("days").innerHTML= 00;
+    //document.getElementById("hours").innerHTML= 00;
+    //document.getElementById("minutes").innerHTML= 00;
+    //document.getElementById("seconds").innerHTML= 00;
+  //}
+ //}, 1000);
+ //Count down timer End
