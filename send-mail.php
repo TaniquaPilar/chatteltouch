eccd034852f836bc69dc6f5e96973b9442c0fcae
TaniquaPilar <chattelboards@gmail.com>
2023-07-12 17:14:04 -0400

    update mail 07/12
    I ran the command: composer require
    phpmailer/phpmailer in the command prompt to get all the composer and vendor files.
    Made a new sent.html

---
+++ send-mail.php
@@ -0,0 +1,36 @@
+ <?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "chattelenterprise@gmail.com";
+$mail->Password = "riqldgmlzrzerykt";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("chattelenterprise@gmail.com");
+
+$mail->Subject = $subject;
+$mail->Body = $email;
+
+
+$mail->send();
+
+header("Location: sent.html");
